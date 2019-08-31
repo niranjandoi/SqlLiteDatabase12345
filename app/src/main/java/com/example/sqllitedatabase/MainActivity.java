0e1bd7ebdf6330857c9ede2d12a64bdf699d581c
Niranjan <niranjanforby96@gmail.com>
2019-08-31 16:33:09 +0530

    sqlite

---
+++ app/src/main/java/com/example/sqllitedatabase/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.sqllitedatabase;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editRollno,editName,editMarks;
+    Button btnAdd,btnDelete,btnModify,btnView,bntViewAll,btnShowinfo;
+    SQLiteDatabase db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editRollno=findViewById(R.id.editRollno);
+        editName=findViewById(R.id.editName);
+        editMarks=findViewById(R.id.marks);
+        btnAdd=findViewById(R.id.buttonAdd);
+        btnDelete=findViewById(R.id.buttonDelete);
+        btnModify=findViewById(R.id.buttonModify);
+        btnView=findViewById(R.id.buttonView);
+        bntViewAll=findViewById(R.id.buttonViewAll);
+        btnShowinfo=findViewById(R.id.buttonshow);
+
+        btnAdd.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnModify.setOnClickListener(this);
+        btnView.setOnClickListener(this);
+        bntViewAll.setOnClickListener(this);
+        btnShowinfo.setOnClickListener(this);
+        db=openOrCreateDatabase("StudentDB" ,MODE_PRIVATE,null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.buttonAdd:
+                if(editRollno.getText().toString().trim().length()==0 || editName.getText().toString().trim().length()==0||
+                        editMarks.getText().toString().trim().length()==0){
+                Toast.makeText(this, "invalid input", Toast.LENGTH_SHORT).show();
+                showmsg("Error", "Invalid Input");
+                return;}
+
+                db.execSQL("INSERT INTO student VALUES('"+editRollno.getText()+"','"+editName.getText()+
+                        "','"+editMarks.getText()+"');");
+                showmsg("Success", "Record added");
+                clearText();
+                break;
+            case R.id.buttonDelete:
+                if(editRollno.getText().toString().trim().length()==0)
+                {
+                    showmsg("Error", "Please enter Rollno");
+                    return;
+                }
+                Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+                if(c.moveToFirst())
+                {
+                    db.execSQL("DELETE FROM student WHERE rollno='"+editRollno.getText()+"'");
+                    showmsg("Success", "Record Deleted");
+                }
+                else
+                {
+                    showmsg("Error", "Invalid Rollno");
+                }
+                clearText();
+                break;
+            case R.id.buttonModify:
+                if(editRollno.getText().toString().trim().length()==0)
+                {
+                    showmsg("Error", "Please enter Rollno");
+                    return;
+                }
+                Cursor c1=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+                if(c1.moveToFirst())
+                {
+                    db.execSQL("UPDATE student SET name='"+editName.getText()+"',marks='"+editMarks.getText()+
+                            "' WHERE rollno='"+editRollno.getText()+"'");
+                    showmsg("Success", "Record Modified");
+                }
+                else
+                {
+                    showmsg("Error", "Invalid Rollno");
+                }
+                clearText();
+                break;
+
+            case R.id.buttonshow:
+                Toast.makeText(this, "Show", Toast.LENGTH_SHORT).show();
+                showmsg("Devloped by niranjan", "NSD");
+                break;
+            case R.id.buttonView:
+                if(editRollno.getText().toString().trim().length()==0)
+                {
+                    showmsg("Error", "Please enter Rollno");
+                    return;
+                }
+                Cursor c2=db.rawQuery("SELECT * FROM student WHERE rollno='"+editRollno.getText()+"'", null);
+                if(c2.moveToFirst())
+                {
+                    editName.setText(c2.getString(1));
+                    editMarks.setText(c2.getString(2));
+                }
+                else
+                {
+                    showmsg("Error", "Invalid Rollno");
+                    clearText();
+                }
+                break;
+
+            case R.id.buttonViewAll:
+
+                Cursor c3=db.rawQuery("SELECT * FROM student", null);
+                if(c3.getCount()==0)
+                {
+                    showmsg("Error", "No records found");
+                    return;
+                }
+                StringBuffer buffer=new StringBuffer();
+                while(c3.moveToNext())
+                {
+                    buffer.append("Rollno: "+c3.getString(0)+"\n");
+                    buffer.append("Name: "+c3.getString(1)+"\n");
+                    buffer.append("Marks: "+c3.getString(2)+"\n\n");
+                }
+                showmsg("Student Details", buffer.toString());
+                break;
+        }
+    }
+
+    private void clearText() {
+        editRollno.setText("");
+        editName.setText("");
+        editMarks.setText("");
+    }
+
+
+    private void showmsg(String error, String invalid_input) {
+        AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
+        alertDialog.setCancelable(true);
+        alertDialog.setTitle(error);
+        alertDialog.setMessage(invalid_input);
+        alertDialog.setIcon(R.mipmap.ic_launcher_round);
+        alertDialog.show();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+}
